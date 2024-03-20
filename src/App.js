9f8d7ad7f0d93a8a360cca8ed74a3e89bf5662be
Ivana Njike <ivananjike@gmail.com>
2024-03-20 12:26:54 +0100

    end home profile;aboutme; resume and projects sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./portfolioContainer/Home/Home";
+import AboutMe from "./portfolioContainer/AboutMe/AboutMe";
+import Resume from "./portfolioContainer/Resume/Resume";
+import Project from "./portfolioContainer/Projects/Project";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
+      <AboutMe/>
+      <Resume/>
+      <Project/>
     </div>
   );
 }
